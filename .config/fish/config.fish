ce71187230fe224a032fa7e3ff1a93b02c5d351f
zhangjiaxing <zhang.jiaxing@foxmail.com>
2017-12-11 16:59:19 +0800

    edit .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,10 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
+alias pd=pushd
+
+set fish_greeting
 
 function fish_user_key_bindings
-    # bind xxx xxx
+    bind '[24~' 'cd ~; commandline -f repaint; commandline -i "pwd"'
 end

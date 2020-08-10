b48d9db0c80c7b29d1e51dc7fb34f987eea8b4da
zhiliao <zhang.jiaxing@foxmail.com>
2020-08-11 00:39:34 +0800

    edit config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,8 @@ set fish_greeting
 function fish_user_key_bindings
     bind '[24~' 'cd ~/myssh/; commandline -f repaint; commandline -i "./myssh.sh sessions.d/"'
 end
+
+# powerline
+set fish_function_path $fish_function_path "/usr/share/powerline/fish"
+powerline-setup
+

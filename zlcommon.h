d0c2aaf29ab6dd50c37755bc65c79bf8b6395144
zhiliao <zhang.jiaxing@foxmail.com>
2022-04-06 10:09:05 +0800

    common.h

--- zlcommon.h
+++ zlcommon.h
@@ -5,7 +5,7 @@
 #define Min(x, y)   ((x) < (y) ? (x) : (y))
 #define Abs(x)      ((x) >= 0 ? (x) : -(x))
 
-#define ErrExit(msg)    do { char msgout[500]; snprintf(msgout, 500, "Error %s: %s:%d %s", msg, __FILE__, __LINE__, __FUNCTION__); perror(msgout); exit(1);} while (0)
+#define DEBUG(fmt, ...) do { fprintf(stderr, "%s: " fmt, __func__, ## __VA_ARGS__); } while (0)
 
 
 #endif /* ZL_COMMON_H */
